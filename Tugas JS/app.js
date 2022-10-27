3106baafe8d1cfa11fe05243ec6232346cc1f416
syahrulramadhans <80559342+syahrulramadhans@users.noreply.github.com>
2022-10-28 00:27:29 +0700

    Add files via upload

---
+++ Tugas JS/app.js
@@ -0,0 +1,24 @@
+//pada consol ini untuk mengirim output string kepada consol yang ada pada browser
+console.log('Hello World'); 
+
+//pada consol ini untuk mengirim output integer kepada consol yang ada pada browser
+console.log('123'); 
+
+//kita juga bisa mengirimkan type boolean ke console
+console.log(true);
+
+//kita juga bisa menampilkan hasil dari suatu variable
+var umur = '22 tahun';
+console.log(umur);
+
+//kita juga bisa menggunakan deretan angka(Array) tiap index dipisahkan dengan tanda koma(,)
+console.log(1, 2, 3, 4);
+
+//table juga bisa dengan menggunakan kurung kurawal ({})
+console.table({a:1, b:2});
+
+//menampilkan error juga bisa
+console.error('Maaf, password ada salah');
+
+//jika ingin memberitahukan atau memunculkan semacam pop up pemberitahuan bisa menggunakan alert
+alert('Wah umur anda masih terlalu muda');
